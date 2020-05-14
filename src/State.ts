30ef1f4091977701b0e8ae7f7de3c84921645251
JulianRaso <julianraso@live.com>
2020-05-13 21:03:49 -0300

    API

--- src/State.ts
+++ src/State.ts
@@ -73,36 +73,6 @@ export class State {
         }
     }
 
-    static Numeros(a,b,c){
-        this.Num1 = a.replace(/\s/g, "");;
-        let Oper = b.replace(/\s/g, "");
-        this.Num2 = c.replace(/\s/g, "");
-
-        if(Oper == "sumar"){
-            this.Num1 = traducir.TraducirNumeros(this.Num1);
-            this.Num2 = traducir.TraducirNumeros(this.Num2);
-            return calculadora.sumar(this.Num1,this.Num2);
-        }
-        else if(Oper == "restar"){
-            this.Num1 = traducir.TraducirNumeros(this.Num1);
-            this.Num2 = traducir.TraducirNumeros(this.Num2);
-            return calculadora.restar(this.Num1,this.Num2);
-        }
-        else if(Oper == "multiplicar"){
-            this.Num1 = traducir.TraducirNumeros(this.Num1);
-            this.Num2 = traducir.TraducirNumeros(this.Num2);
-            return calculadora.multiplicar(this.Num1,this.Num2);
-        }
-        else if(Oper == "dividir"){
-            this.Num1 = traducir.TraducirNumeros(this.Num1);
-            this.Num2 = traducir.TraducirNumeros(this.Num2);
-            return calculadora.dividir(this.Num1,this.Num2);
-        }
-        else{
-            return this.err;
-        }
-    }
-
 }
 
 export default State;
